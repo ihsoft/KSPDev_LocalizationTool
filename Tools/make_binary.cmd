4956db3449367365a93ff046f6d8f8af242d724a
ihsoft <igor.zavoychinskiy@gmail.com>
2018-07-09 21:48:08 -0700

    Fix target cleaning when making release

--- Tools/make_binary.cmd
+++ Tools/make_binary.cmd
@@ -1,3 +1,3 @@
 @echo off
 @REM Until new major version of C# is released the build number in the path will keep counting.
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild ..\Source\KSPDev_LocalizationTool.csproj /t:Rebuild /p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild ..\Source\KSPDev_LocalizationTool.csproj /t:Clean,Build /p:Configuration=Release
